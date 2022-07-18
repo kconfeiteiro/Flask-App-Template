3ce9cd71ed408836f1ab170caae867bea83227e4
Krystian Confeiteiro <confeitk@my.erau.edu>
2022-07-18 07:42:36 -0400

    stuff DID work finally

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home(): 
-    return render_template("home")
+    return render_template("home.html")
 
 @app.route("/theory")
 def theory(): 
