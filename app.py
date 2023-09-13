91e77a91053d25b0848f657ff515260c072bbba1
Krystian Ojeda Confeiteiro <confeitk@my.erau.edu>
2023-09-12 21:08:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,6 @@ app = Flask(__name__)
 @app.route("/home")
 def home(): 
     return render_template("home.html")
-
-@app.route("/theory")
-def theory(): 
-    return render_template("theory.html")
-
-@app.route("/navigation")
-def navigation():
-    return render_template("navigation.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
